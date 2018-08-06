64730860c941e0605cea9cd6d9115820fd7be725
andrey voronov <andreyworonov@yahoo.com>
2018-08-06 11:08:01 +0300

    make modal window

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let form = document.querySelector('.formWithValidation');
 let userCoordinates = form.querySelector('.userCoordinates');
 let userResult = document.querySelector('.resultMessage');
+let modal = document.querySelector(".modal");
+let closeButton = document.querySelector(".close-button");
 let countries;
 
 // on search click events
@@ -10,19 +12,43 @@ form.addEventListener('submit', function (event) {
     validateUserInput();
 })
 
+// close modal events
+closeButton.addEventListener("click", openCloseModal);
+window.addEventListener("click", windowOnClick);
+
 function validateUserInput() {
     let userX;
     let userY;
-    
-    if (userCoordinates.value.match(/\x=[0-9]*/)) {
-        userX = Number(userCoordinates.value.match(/\x=[0-9]*/)[0].substring(2));
-    };
-    if (userCoordinates.value.match(/\y=[0-9]*/)) {
-        userY = Number(userCoordinates.value.match(/\y=[0-9]*/)[0].substring(2));
-    };
-
-    findClosestCountry(userX, userY, countries)
+    if (userCoordinates.value.match(/^x=[0-9]+$|y=[0-9]+$|x=[0-9]+[,]y=[0-9]+$|y=[0-9]+[,]+x=[0-9]+$/)) {
+        if (userCoordinates.value.match(/\x=[0-9]*/)) {
+            userX = Number(userCoordinates.value.match(/\x=[0-9]*/)[0].substring(2));
+        };
+        if (userCoordinates.value.match(/\y=[0-9]*/)) {
+            userY = Number(userCoordinates.value.match(/\y=[0-9]*/)[0].substring(2));
+        };
+        
+        findClosestCountry(userX, userY, countries)
+    }else {
+        let modalIsOpen = false;
+        openCloseModal(modalIsOpen);
+    }
 }
+
+function openCloseModal(modalIsOpen) {
+    if(modalIsOpen === false) {
+        modalIsOpen = true;
+        modal.classList.add("show-modal");
+    }else {
+        modal.classList.remove("show-modal");
+    }
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        modal.classList.remove("show-modal");
+    }
+}
+
 
 fetch('countries.json')
     .then(function (response) { 
